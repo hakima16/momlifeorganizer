61a8ef20e4a572a9f36c912bf04849d55cd803b2
hakima16 <chhakima87@gmail.com>
2026-05-10 18:20:00 -0400

    Update cleaningService.js

--- client/src/services/cleaningService.js
+++ client/src/services/cleaningService.js
@@ -1,14 +1,19 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/cleaning";
+const API_URL = "https://momlifeorganizer.onrender.com/api/cleaning";
 
 export const getCleaningTasks = async () => {
   const res = await axios.get(API_URL);
+  return Array.isArray(res.data) ? res.data : [];
+};
+
+export const createCleaningTask = async (data) => {
+  const res = await axios.post(API_URL, data);
   return res.data;
 };
 
-export const createCleaningTask = async (taskData) => {
-  const res = await axios.post(API_URL, taskData);
+export const updateCleaningTask = async (id, data) => {
+  const res = await axios.put(`${API_URL}/${id}`, data);
   return res.data;
 };
 
@@ -16,3 +21,4 @@ export const deleteCleaningTask = async (id) => {
   const res = await axios.delete(`${API_URL}/${id}`);
   return res.data;
 };
+
